30d8d95eae286ed33047bd29f22e9bc6168c2e6c
Edwin Cheng <edwincheng4840@gmail.com>
2024-11-17 22:57:45 -0500

    q4 test case

--- prog4.sh
+++ prog4.sh
@@ -2,11 +2,13 @@
 
 SRC_DIR="$1"
 
+# If SRC_DIR doesn't exist, then error
 if [ ! -d "$SRC_DIR" ]; then
     echo "$SRC_DIR is not a directory"
     exit 1
 fi
 
+# If input isn't exactly 1, then error
 if [ "$#" -ne 1 ]; then
     echo "score directory is missing"
     exit 1
@@ -16,23 +18,27 @@ for file in "$SRC_DIR"/*; do
     awk '
     BEGIN {
         FS=","
+        # Count number of students
         student_count = 0
     }
+    # Ignore first row of metadata, and parse second line of data
     NR > 1 {
         student_count += 1
         student_id[student_count] = $1
         grade_sum = 0
+        # Obtain grade out of 50
         for (i = 2; i <= NF; i++) {
             grade_sum += $i
-            field_value = $i   
-            # print "Field " i ": " field_value 
+            field_value = $i
         }
-        percent_grade = grade_sum*2
-        if (percent_grade >= 93) {
+        # Obtain percent grade out of 100 and translate into letter grade
+        percent_grade = (grade_sum*2)
+        rounded_grade = int(percent_grade+0.5)
+        if (rounded_grade >= 93) {
             grade = "A"
-        } else if (percent_grade >= 80) {
+        } else if (rounded_grade >= 80) {
             grade = "B"
-        } else if (percent_grade >= 65) {
+        } else if (rounded_grade >= 65) {
             grade = "C"
         } else {
             grade = "D"
