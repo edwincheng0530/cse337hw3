418c8bd0f50c2511a6dee4ba176349a4ae4cfe30
Edwin Cheng <edwincheng4840@gmail.com>
2024-11-17 23:06:33 -0500

    q5 test case

--- prog5.sh
+++ prog5.sh
@@ -3,23 +3,27 @@
 SRC_FILE="$1"
 DIC_FILE="$2"
 
+# If there aren't exactly 2 inputs, error
 if [ "$#" -ne 2 ]; then
     echo "input file and dictionary missing"
     exit 1
 fi
 
+# If given SRC_FILE isn't a .txt file, error
 if [ ! -f "$SRC_FILE" ] && [[ "$SRC_FILE" == *.txt ]]; then
     echo "$SRC_FILE is not a file"
     exit 1
 fi
 
-if [ ! -f "$DIC_FILE" ]&& [[ "$DIC_FILE" == *.txt ]]; then
+# If given DIC_FILE isn't a .txt file, error
+if [ ! -f "$DIC_FILE" ] && [[ "$DIC_FILE" == *.txt ]]; then
     echo "$DIC_FILE is not a file"
     exit 1
 fi
 
+# Find only matching part of regex for each line
 grep -o '\b[a-zA-Z]\{4\}\b' "$SRC_FILE" | while read word; do
-    # Convert to lowercase for case-insensitive comparison
+    # Convert to lowercase for case-insensitive comparison against dictionary
     word_lower=$(echo "$word" | tr '[:upper:]' '[:lower:]')
     
     # Check if the word exists in the dictionary
